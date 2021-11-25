86b26f2e284d90dfd3e86b692f24b79ab73c5d01
Durai Aravindhraj Elangovan <83654766+duraiaravindh@users.noreply.github.com>
2021-11-25 19:42:39 +0530

    Add files via upload

---
+++ class.js
@@ -0,0 +1,23 @@
+function myfunc(){
+    let elem = document.getElementById("myTable");
+    let row = elem.insertRow();
+    let cell1 = row.insertCell();
+    let cell2 = row.insertCell();
+    let cell3 = row.insertCell();
+    let cell4 = row.insertCell();
+    let cell5 = row.insertCell();
+    let cell6 = row.insertCell();
+    let cell7 = row.insertCell();
+    let cell8 = row.insertCell();
+    // cell1.innerHTML = document.getElementById("fName").value;
+    // cell2.innerHTML = document.getElementById("LName").value;
+    // cell3.innerHTML = document.getElementById("address").value;
+    // cell4.innerHTML = document.getElementById("pincode").value;
+    // cell5.innerHTML = document.getElementById("gender").value;
+    // cell6.innerHTML = document.getElementById("food").value;
+    // cell7.innerHTML = document.getElementById("state").value;
+    // cell8.innerHTML = document.getElementById("country").value;
+  cell1.innerHTML = "durai"
+    cell2.innerHTML = "aravindhraj"
+
+};
